94996d864dac8ca93a9ebf3e48344003d974fd77
PucheCarlos <puchecarlos00@gmail.com>
2024-06-06 22:14:40 -0400

    escritorio

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const nav = document.querySelector(".nav");
 const closeIcon = document.querySelector(".close");
 const menuIcon = document.querySelector(".menuIcon");
+const details = document.querySelectorAll(".details");
+const arrows = document.querySelectorAll(".arrow");
 
+
+for (let cont = 0; cont < details.length; cont++) {
+    details[cont].addEventListener("click", ()=>{
+        if (!details[cont].hasAttribute("open")){
+            arrows[cont].src = "./images/icon-arrow-up.svg";
+        }
+        else {
+            arrows[cont].src = "./images/icon-arrow-down.svg";
+        }
+    })
+}
+/************************************* Movil Events ********************************************** */
 menuIcon.addEventListener("click", ()=>{
-    // nav.style.transform = "translateX(-10rem);"
-    nav.setAttribute('style', 'margin: 0 0%; visibility: visible; ');
+    nav.classList.remove("none");
+    setTimeout(()=>{
+        nav.setAttribute('style', 'margin: 0 0%;');
+    },250);
    
 });
 
 closeIcon.addEventListener("click", ()=>{
-    // nav.style.transform = "translateX(-10rem);"
-    nav.setAttribute('style', 'margin: 0 100%; visibility: hidden; ');
+    nav.setAttribute('style', 'margin: 0 110%;');
+    setTimeout(()=>{
+        nav.classList.add("none");
+    },800);
 });
 
 
-console.log(nav);
-// console.log(closeIcon);
-// console.log(menuIcon);
+const img = document.querySelector(".img");
